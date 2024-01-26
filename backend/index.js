7b8f8627a3d9eeb4530a4f3d9a7b45288e0bfce0
Fábio Costa <fabiodanielmc@gmail.com>
2024-01-26 22:38:45 +0100

    create route for new book

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import mongoose, { mongo } from "mongoose";
 import { Book } from "./models/bookModel.js";
 
 const app = express();
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) =>{
     console.log(request)
@@ -14,6 +16,24 @@ app.get('/', (request, response) =>{
 
 app.post('/books', async (request, response) =>{
     try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publish year"
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        }
+        const book= await Book.create(newBook);
+
+        return response.status(201).send(book);
+
     } catch (error){
         console.log(error.message);
         response.status(500).send({message: error.message})
